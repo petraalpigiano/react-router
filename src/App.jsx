ca68a11a28525e8c4b8c4934eddf9894e94dca99
petraalpigiano <petraalpigiano@gmail.com>
2025-06-02 21:21:08 +0200

    add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-export default function App() {}
+import HomePage from "./pages/HomePage";
+import ContactPage from "./pages/ContactPage";
+import PostsPage from "./pages/PostsPage";
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/contact" element={<ContactPage />}></Route>
+        <Route path="/posts" element={<PostsPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
