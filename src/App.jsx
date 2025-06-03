77ceb30a5a6f5b3d7ad5bd3c609a1bc72367f3a6
petraalpigiano <petraalpigiano@gmail.com>
2025-06-03 15:35:44 +0200

    add notfoundpage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import HomePage from "./pages/HomePage";
 import ContactPage from "./pages/ContactPage";
 import PostsPage from "./pages/PostsPage";
 import DefaultLayout from "./layouts/DefaultLayout";
+import NotFoundPage from "./pages/NotFoundPage";
+
 export default function App() {
   return (
     <BrowserRouter>
@@ -11,6 +13,7 @@ export default function App() {
           <Route path="/" element={<HomePage />}></Route>
           <Route path="/contact" element={<ContactPage />}></Route>
           <Route path="/posts" element={<PostsPage />}></Route>
+          <Route path="*" element={<NotFoundPage />}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
