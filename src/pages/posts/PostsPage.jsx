5a3fc5d884654645a0c6a411fcaed6509f85005c
petraalpigiano <petraalpigiano@gmail.com>
2025-06-03 15:59:27 +0200

    add backend call

--- src/pages/posts/PostsPage.jsx
+++ src/pages/posts/PostsPage.jsx
@@ -1,4 +1,12 @@
+import axios from "axios";
+import { useEffect } from "react";
 export default function PostsPage() {
+  useEffect(() => {
+    axios.get("http://localhost:3000/posts").then((res) => {
+      console.log(res.data);
+    });
+  }, []);
+
   return (
     <>
       <h1>Sono la Post</h1>
