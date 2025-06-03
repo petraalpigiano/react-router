ba89f10af57f8a47df2778a5f543dcca043f03a0
petraalpigiano <petraalpigiano@gmail.com>
2025-06-03 15:50:54 +0200

    add postpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ContactPage from "./pages/ContactPage";
-import PostsPage from "./pages/PostsPage";
+import PostsPage from "./pages/posts/PostsPage";
 import DefaultLayout from "./layouts/DefaultLayout";
 import NotFoundPage from "./pages/NotFoundPage";
 
@@ -9,10 +9,16 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* ROTTE CON IN COMUNE IL LAYOUT */}
         <Route element={<DefaultLayout />}>
           <Route path="/" element={<HomePage />}></Route>
           <Route path="/contact" element={<ContactPage />}></Route>
-          <Route path="/posts" element={<PostsPage />}></Route>
+
+          {/* ROTTE CON IN COMUNE IL PREFISSO  */}
+          <Route path="/posts">
+            <Route path="" element={<PostsPage />}></Route>
+          </Route>
+
           <Route path="*" element={<NotFoundPage />}></Route>
         </Route>
       </Routes>
